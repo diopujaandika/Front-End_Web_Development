b35522ec5efbc1524217c1f53b575d086dfaeb77
diopujaandika <dpujaandika17@gmail.com>
2025-11-06 22:35:54 +0700

    how to recalll BOM member

---
+++ Beginner/1_BOM_DOM/7_Cara_Memanggil_BOM/1_memanggil.js
@@ -0,0 +1,10 @@
+// Cara memanggil anggota BOM
+
+/*
+*/
+
+    // Cara pertama
+    window.alert('Hello World!');   // Lebih disarankan karena scope bersifat globa
+
+    // Cara kedua
+    alert('Hello Universe!');   // Rawan terjadi bug karena scopenya bisa berda-beda.
