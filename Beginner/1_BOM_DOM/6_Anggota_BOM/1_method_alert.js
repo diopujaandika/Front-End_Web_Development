695118ecdaf5d756a8942780a0fde155360fdd00
diopujaandika <dpujaandika17@gmail.com>
2025-11-06 21:48:09 +0700

    learn about browser environment and method alert in BOM and DOM with JS

---
+++ Beginner/1_BOM_DOM/6_Anggota_BOM/1_method_alert.js
@@ -0,0 +1,10 @@
+// METHOD ALERT
+/*
+    Method alert() merupakan salah satu method BOM yang menerima satu parameter opsional berupa data string yang akan ditampilkan melalui sebuah opo-up browser.
+*/
+
+alert('Hallo, user!');
+let user = 'Dio Puja Andika';
+let message = `Hallo ${user}`;
+
+alert(message);
