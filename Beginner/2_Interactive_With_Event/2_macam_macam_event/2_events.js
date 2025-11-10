d8310715c70024e3514ccfc992accc8156dbf770
diopujaandika <dpujaandika17@gmail.com>
2025-11-10 16:34:46 +0700

    update make event in js

---
+++ Beginner/2_Interactive_With_Event/2_macam_macam_event/2_events.js
@@ -0,0 +1,42 @@
+// Window Events
+/*
+    Window Events adalah kejadian-kejadian yang berasal dari browser alias pada window.
+
+    1. onload => Event ini akan terjadi setelah halaman web selesai diunduh dan ditampilkan.
+    2. onpageshow => Event ini akan  terjadi jika halaman web kembali dikunjungi kembali (berbeda dengan onload, kerena event onload tidak akan terjadi jika halaman web diambil dari cache).
+    3. onresize => Event ini akan terjadi window browser di-resize oleh user atau hal lainnya.
+*/
+
+// Form Events
+/*
+    Form events adalah kerjadian-kejadian yang berasal dari sebuah elemen HTML dengan tag <form> (kita akan mempraktikkan penggunaan tag <form> pada materi Form Event).
+    1. onreset => Event akan terjadi ketika user menekan tombol 'reset" pada elemen form.
+    2. onsubmit => Event ini akan ketika user menekan tombol 'submit" pada form.
+*/
+
+// Keyboard Events
+/*
+    Keyboard Events adalah kejadian-kejadian yang berasal dari ditekan atau dilepasnya tombol pada keyboard.
+
+    1. onkeydown => Event ini akan terjadi ketika sebuah tombol pada keyboard ditekan.
+    2. onkeypress =>  Event ini terjadi setalah onkeydown, di mana tombol masih dalam keadaan ditekan. Event ini tidak dapat mendeteksi tombol fungsi seperti CTRL, SHIFT, ALT, ESC, BACKSPACE, dan sebagiannya.
+    3. onkeyup => Event ini akan terjadi ketika sebuah tombol pada keyboard dilepas.
+*/
+
+// Clipboard Events
+/* 
+    Clipboard Events adalah kejadian-kejadian yang berasal dari proses cut, copy, atau paste sebuah elemen.
+
+    1. oncopy => Event ini akan terjadi ketika kita melakukan proses copy.
+    2. oncut => Event ini akan terjadi ketika kita melakukan proses cut.
+    3. onpaste => Event ini akan terjadi kita melalukan proses paste.
+*/
+
+// Mouse Events
+/*
+    Mouse Events adalah kejadian-kejadian yang berasal dari kegiatan klik mouse.
+
+    1. onclick => Event ini akan terjadi ketika kita menekan elemen tertentu dengan cursor.
+    2. ondbclick => Event ini akan terjadi ketika kita menekan elemen tertentu dua kali berturut-turut dengan cursor.
+    3. onmouseover => Event ini akant erjadi ketika cursor kita berada di atas sebuah elemen tetentu.
+*/
