809382fc90c965000639a125df1590f799a282de
diopujaandika <dpujaandika17@gmail.com>
2025-11-15 22:51:27 +0700

    update

--- Beginner/1_BOM_DOM/8_DOM/script.js
+++ Beginner/1_BOM_DOM/8_DOM/script.js
@@ -7,4 +7,13 @@ judul.innerHTML = 'Dio Puja Andika'
 
 // document.getElementTagName(); => Return HTML Collection
 const paragraf = document.getElementsByTagName('p');
-paragraf[3].style.backgroundColor = 'lightblue';
+for(let i = 0; i < paragraf.length; i++) {
+    paragraf[i].style.backgroundColor = 'lightblue';
+}
+
+const h1 = document.getElementsByTagName('h1')[0];
+h1.style.fontSize = '50px';
+
+// document.getElementByClassName(); => HTMLCollection
+const p1 = document.getElementsByClassName('p1')[0];
+p1.innerHTML = 'Ini diubah dari JavaScript';
